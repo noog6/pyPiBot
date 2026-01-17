6a1860decc490c6cde186afc28cd7d658b55e604
N C <noog6@users.noreply.github.com>
2026-01-17 10:19:39 -0500

    Add PCA9685 servo controller and actuator

--- hardware/__init__.py
+++ hardware/__init__.py
@@ -1,1 +1,6 @@
-"""Package placeholder for hardware modules."""
+"""Hardware controller package."""
+
+from hardware.pca9685_servo_controller import PCA9685Actuator
+from hardware.servo_actuator import ServoActuator
+
+__all__ = ["PCA9685Actuator", "ServoActuator"]
