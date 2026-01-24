083d11eeeb4fb1c4411d78374f2bcf403e80f039
N C <noog6@users.noreply.github.com>
2026-01-24 14:47:23 -0500

    Add gesture library framework

--- motion/__init__.py
+++ motion/__init__.py
@@ -2,7 +2,15 @@
 
 from motion.action import Action
 from motion.keyframe import Keyframe
+from motion.gesture_library import GestureLibrary
 from motion.gestures import gesture_idle, gesture_nod
 from motion.motion_controller import MotionController
 
-__all__ = ["Action", "Keyframe", "MotionController", "gesture_idle", "gesture_nod"]
+__all__ = [
+    "Action",
+    "GestureLibrary",
+    "Keyframe",
+    "MotionController",
+    "gesture_idle",
+    "gesture_nod",
+]
