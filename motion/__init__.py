ef16ec4d5d4973af47fc09546d8fd9de49cf04be
N C <noog6@users.noreply.github.com>
2026-01-24 14:22:22 -0500

    Add basic gesture builders for idle and nod

--- motion/__init__.py
+++ motion/__init__.py
@@ -2,6 +2,7 @@
 
 from motion.action import Action
 from motion.keyframe import Keyframe
+from motion.gestures import gesture_idle, gesture_nod
 from motion.motion_controller import MotionController
 
-__all__ = ["Action", "Keyframe", "MotionController"]
+__all__ = ["Action", "Keyframe", "MotionController", "gesture_idle", "gesture_nod"]
